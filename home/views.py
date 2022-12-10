4d092c92fc17c413ff74678e6ac3d8a2acf5ee53
receperd <recep.2001.erdogan@gmail.com>
2022-12-10 23:06:45 +0300

    1

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    #text="merhaba dünya"
+    #return HttpResponse("ilk django projesi %s." % text)
+    text='text metni ';
+    context={'text':text}
+    return render(request, 'index.html', context)
