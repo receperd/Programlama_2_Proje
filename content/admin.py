295ebbcbb66fa11f45916655e6892eae3327d993
receperd <recep.2001.erdogan@gmail.com>
2022-12-11 23:56:57 +0300

    1

--- content/admin.py
+++ content/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 
-from content.models import Category
+from content.models import Category, Content, Images
+
 
 # Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 class CategoryAdmin(admin.ModelAdmin):
-    list_display=['title','status','image_tag']
-    list_filter=['status','image']
+    list_display=['title','status']
+    list_filter=['status']
 
-admin.site.register(Category,CategoryAdmin)
+class ContentAdmin(admin.ModelAdmin):
+    list_display=['title','category','image_tag','status']
+    readonly_fields = ('image_tag',)
+    list_filter=['status','category']
+    inlines = [ProductImageInline]
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display=['title','content','image']
+
+
+admin.site.register(Category,CategoryAdmin) #categorynin admin panelinde gösterilmesi
+admin.site.register(Content,ContentAdmin) #contentin admin panelinde gösterilmesi
+admin.site.register(Images,ImagesAdmin) #imagesin admin panelinde gösterilmesi
--- content/models.py
+++ content/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+
 
 # Create your models here.
 
@@ -20,3 +22,37 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+class Content(models.Model):
+    STATUS = (
+        ('True','Evet'),
+        ('False', 'Hayır'),
+    )
+    category=models.ForeignKey(Category, on_delete=models.CASCADE) #category tablosu ile ilişkilendirme
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    image = models.ImageField(blank=True, upload_to='images/')
+    file = models.FileField(blank=True, upload_to='files/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    pub_date = models.DateTimeField('date published')
+    slug=models.SlugField()
+
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+
+
+class Images(models.Model):
+    content=models.ForeignKey(Content,on_delete=models.CASCADE)
+    title=models.CharField(max_length=50)
+    image = models.ImageField(blank=True, upload_to='images/')
+    def __str__(self):
+        return self.title
+
